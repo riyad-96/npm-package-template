5d7318bc5c06c341612437b31ec6ddb01a38de6c
riyad-96 <riyadofficial.ide@gmail.com>
2026-05-14 16:36:04 +0600

    Initialized npm-package-template

---
+++ vite.config.ts
@@ -0,0 +1,41 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import dts from 'vite-plugin-dts';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    react(),
+    dts({
+      tsconfigPath: './tsconfig.json',
+      outDir: 'dist',
+      entryRoot: 'src',
+      rollupTypes: false,
+      cleanVueFileName: true,
+    }),
+  ],
+  build: {
+    emptyOutDir: true,
+    lib: {
+      entry: {
+        index: path.resolve(__dirname, 'src/index.ts'),
+      },
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        preserveModules: true,
+        preserveModulesRoot: 'src',
+        entryFileNames: ({ name }) => `${name}.js`,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
